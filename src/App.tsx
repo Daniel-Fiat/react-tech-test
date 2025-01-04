e37a1fc3a688aaa676720895a7ac7edbc6310d82
pepealacid <daniel.fiat@armadilloamarillo.com>
2025-01-04 16:49:49 +0100

    fix(global css): correct styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/routes"; // Asegúrate de importar correctamente las rutas
-import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = (): JSX.Element => {
   return (
